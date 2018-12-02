6787eb1fff8d6232c70777bdfa9432977d74301e
semihokanpehlivan <semihokanp@gmail.com>
2018-12-03 00:20:18 +0300

    noted the tricky multiple config load order.

--- sopehl-property-source/src/main/java/com/sopehl/properties/MultipleConfig.java
+++ sopehl-property-source/src/main/java/com/sopehl/properties/MultipleConfig.java
@@ -4,12 +4,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
 import org.springframework.stereotype.Component;
-
+//Got more details from https://www.mkyong.com/spring/spring-propertysources-example/
 @Component
 @PropertySources({
         @PropertySource("classpath:config.properties"),
         @PropertySource(value = "classpath:missing.properties", ignoreResourceNotFound = true),
-        @PropertySource("classpath:other.properties")
+        @PropertySource("classpath:other.properties")//if same key, this will 'win'
 })
 public class MultipleConfig {
 
