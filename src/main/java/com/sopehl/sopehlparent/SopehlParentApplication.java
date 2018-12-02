1587aacd26b03b63f06b118d894d43ff967358ce
semihokanpehlivan <semihokanp@gmail.com>
2018-12-02 22:35:25 +0300

    added initial project files

---
+++ src/main/java/com/sopehl/sopehlparent/SopehlParentApplication.java
@@ -0,0 +1,12 @@
+package com.sopehl.sopehlparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SopehlParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SopehlParentApplication.class, args);
+	}
+}
